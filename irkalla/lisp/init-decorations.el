cb5f6c8a38214d7ad31208b7095965db3fc4fd4e
Icy-Thought <icy-thought@pm.me>
2023-02-28 11:29:31 +0100

    [𝚫] (irkalla): fixed font-lock not being applied

--- irkalla/lisp/init-decorations.el
+++ irkalla/lisp/init-decorations.el
@@ -1,52 +1,55 @@
 ;;; lisp/init-decorations.el -*- lexical-binding: t -*-
 
-(defun set-face-attributes! (face attrs)
-  "Sets the specified ATTRS on the given FACE."
-  (when (facep face)
-    (apply #'set-face-attribute face nil attrs)))
-
-(defun custom-set-faces! (face-specs)
-  "Applies a list of FACE-SPECS in the form of (FACE . ATTRS) to set custom faces."
-  (dolist (face-spec face-specs)
-    (let ((face (car face-spec))
-          (attrs (cdr face-spec)))
-      (set-face-attributes! face attrs))))
-
 ;; Making our beloved font more aesthetically pleasing!
-(custom-set-faces!
- '((default :family "VictorMono Nerd Font" :height 125 :weight semi-bold)
-   (fixed-pitch :family "VictorMono Nerd Font" :height 75 :weight semi-bold)
-   (variable-pitch :family "VictorMono Nerd Font" :height 75 :weight semi-bold)))
+(set-face-attribute 'default nil 
+                    :family "VictorMono Nerd Font"
+                    :height 125
+                    :weight 'semi-bold)
 
-;; Change of slant style
-(custom-set-faces!
- '((font-lock-builtin-face :slant italic)
-   (font-lock-comment-face :slant italic)
-   (font-lock-function-name-face :weight bold :slant italic)
-   (font-lock-keyword-face :slant italic)))
+(set-face-attribute 'variable-pitch nil
+                    :family "VictorMono Nerd Font"
+                    :height 1.0
+                    :weight 'semi-bold)
+
+(set-face-attribute 'fixed-pitch nil
+                    :family "VictorMono Nerd Font"
+                    :height 1.0
+                    :width 'expanded
+                    :weight 'semi-bold)
+
+(set-fontset-font "fontset-default"
+                  'arabic (font-spec :family "Scheherazade New;" :size 25))
+
+;; Turning several font styles -> italic
+(custom-set-faces
+ '(font-lock-builtin-face ((t (:slant italic))))
+ '(font-lock-comment-face ((t (:slant italic))))
+ '(font-lock-doc-face ((t (:slant italic))))
+ '(font-lock-function-name-face ((t (:weight bold :slant italic))))
+ '(font-lock-keyword-face ((t (:slant italic))))
+ '(font-lock-preprocessor-face ((t (:weight bold))))
+ '(font-lock-string-face ((t (:slant italic))))
+)
 
 ;; (Org-Mode): Quotes shall be italic!
 (setq org-fontify-quote-and-verse-blocks t)
 
 ;; (Org-Mode): Different headline sizes!
-(custom-set-faces!
- '((org-document-title :height 1.2)
-   (outline-1 :weight extra-bold :height 1.25)
-   (outline-2 :weight bold :height 1.15)
-   (outline-3 :weight bold :height 1.12)
-   (outline-4 :weight semi-bold :height 1.09)
-   (outline-5 :weight semi-bold :height 1.06)
-   (outline-6 :weight semi-bold :height 1.03)
-   (outline-8 :weight semi-bold)
-   (outline-9 :weight semi-bold)))
+(custom-set-faces
+  '(org-document-title ((t (:height 1.5))))
+  '(org-level-1 ((t (inherit outline-1 :height 1.25))))
+  '(org-level-2 ((t (inherit outline-2 :height 1.15))))
+  '(org-level-3 ((t (inherit outline-3 :height 1.12))))
+  '(org-level-4 ((t (inherit outline-4 :height 1.09))))
+  '(org-level-5 ((t (inherit outline-5 :height 1.06)))))
 
 ;;(Markdown): Different headline sizes!
-(custom-set-faces!
- '((markdown-header-face-1 :height 1.25 :weight extra-bold :inherit markdown-header-face)
-   (markdown-header-face-2 :height 1.15 :weight bold       :inherit markdown-header-face)
-   (markdown-header-face-3 :height 1.08 :weight bold       :inherit markdown-header-face)
-   (markdown-header-face-4 :height 1.00 :weight bold       :inherit markdown-header-face)
-   (markdown-header-face-5 :height 0.90 :weight bold       :inherit markdown-header-face)
-   (markdown-header-face-6 :height 0.75 :weight extra-bold :inherit markdown-header-face)))
+(custom-set-faces
+  '(markdown-header-face-1 ((t (:inherit markdown-header-face :height 1.25 :weight extra-bold))))
+  '(markdown-header-face-2 ((t (:inherit markdown-header-face :height 1.15 :weight bold))))
+  '(markdown-header-face-3 ((t (:inherit markdown-header-face :height 1.08 :weight bold))))
+  '(markdown-header-face-4 ((t (:inherit markdown-header-face :height 1.00 :weight bold))))
+  '(markdown-header-face-5 ((t (:inherit markdown-header-face :height 0.90 :weight bold))))
+  '(markdown-header-face-6 ((t (:inherit markdown-header-face :height 0.75 :weight extra-bold)))))
 
 (provide 'init-decorations)
