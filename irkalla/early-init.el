cc0cc635808f23f15ed322f9c5d1c3a8808c5ea3
Icy-Thought <icy-thought@pm.me>
2023-03-14 15:53:48 +0100

    [𝚫] (irkalla): quicker launch settings <- doomemacs!

--- irkalla/early-init.el
+++ irkalla/early-init.el
@@ -33,6 +33,32 @@
   (add-to-list 'native-comp-eln-load-path
                (expand-file-name "eln-cache/" user-emacs-directory)))
 
+;; Reduce noise on cold-start.
+(setq inhibit-startup-screen t
+      inhibit-startup-echo-area-message user-login-name)
+
+;; Get rid of "For information about GNU Emacs..." message on startup.
+(advice-add #'display-startup-echo-area-message :override #'ignore)
+(advice-add #'display-startup-screen :override #'ignore)
+
+;; Quicker startup when in `fundamental-mode`!
+(setq initial-major-mode 'fundamental-mode
+      initial-scratch-message nil)
+
+(unless init-file-debug
+  ;; Get rid of 'loading X file' echo..
+  (define-advice load-file (:override (file) silence)
+    (load file nil 'nomessage))
+  ;; Undo advice until later for proper loading..
+  (define-advice startup--load-user-init-file (:before (&rest _) undo-silence)
+    (advice-remove #'load-file #'load-file@silence))
+
+  ;; Disable default Emacs mode-line for a minor launch time boost. (30 - 50ms)
+  (put 'mode-line-format 'initial-value (default-toplevel-value 'mode-line-format))
+  (setq-default mode-line-format nil)
+  (dolist (buf (buffer-list))
+    (with-current-buffer buf (setq mode-line-format nil))))
+
 ;;; UI configuration
 ;; Remove bloated UI elements
 (push '(tool-bar-lines . 0) default-frame-alist)
@@ -42,9 +68,6 @@
 ;; Set transparency early
 (add-to-list 'default-frame-alist '(alpha-background . 85))
 (set-frame-parameter (selected-frame) 'alpha-background 85)
-
-;; Add frame borders and window dividers (TODO: apply alpha)
-;; (add-to-list 'default-frame-alist '(internal-border-width . 25))
 
 ;; Avoid flashing white-screen on start
 (load-theme 'modus-vivendi t)
