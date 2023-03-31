1962e360a4bf87f6ef4d96b382f49fcc36d0e471
Icy-Thought <icy-thought@pm.me>
2023-03-31 23:43:35 +0200

    [𝚫] (irkalla): new magit packages + settings

--- irkalla/plugins/toolset/init-magit.el
+++ irkalla/plugins/toolset/init-magit.el
@@ -13,4 +13,42 @@
   (magit-auto-revert-mode nil)
   (magit-display-buffer-function #'magit-display-buffer-fullframe-status-v1))
 
+;; Showing our project TODO's in Magit
+(use-package magit-todos
+  :hook (magit-mode . magit-todos-mode)
+  :init
+  (setq magit-todos-nice (if (executable-find "nice") t nil)
+        magit-todos-scanner #'magit-todos--scan-with-rg)
+  :custom
+  (magit-todos-recursive t)
+  (magit-todos-depth 10)
+  (magit-todos-exclude-globs '(".git/" "*.html"))
+  :config (custom-set-variables
+           '(magit-todos-keywords (list "TODO" "FIXME" "HACK"))))
+
+;; ~git blame~ tool for our setup
+(use-package blamer
+  :defer t
+  :custom
+  (blamer-idle-time 0.5)
+  (blamer-min-offset 70)
+  (blamer-view 'overlay-right)
+  (blamer-type 'visual)
+  (blamer-max-commit-message-length 70)
+  (blamer-force-truncate-long-line nil)
+  (blamer-author-formatter " ✎ %s ")
+  (blamer-commit-formatter "● \'%s\' ● ")
+  :custom-face
+  (blamer-face ((t :foreground "#7a88cf"
+                   :background nil
+                   :height 125
+                   :italic t))))
+
+;; Live highlight of buffer (~git~) changes
+(use-package git-gutter
+  :hook (prog-mode . git-gutter-mode)
+  :diminish git-gutter-mode
+  :config (setq git-gutter:update-interval 1))
+
+
 (provide 'init-magit)
