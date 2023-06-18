a2dabfa676af2b979d301ae757054d10ecb625a9
Icy-Thought <icy-thought@pm.me>
2023-06-18 23:37:32 +0200

    [𝚫] (irkalla): almost complete, perf. diff..??

--- irkalla/plugins/essentials/init-essentials.el
+++
@@ -1,9 +0,0 @@
-;;; essentials/init-essentials.el -*- lexical-binding: t -*-
-
-;; Remaining toolset modules!
-(require 'init-general)
-(require 'init-whichkey)
-(require 'init-helpful)
-;; (require 'init-hydra)
-
-(provide 'init-essentials)
