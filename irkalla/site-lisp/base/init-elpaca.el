561793f1d52a1babfabdb4f161e1675b90dac1ba
Icy-Thought <icy-thought@pm.me>
2023-11-07 09:22:49 +0100

    [𝚫] (irkalla): elpaca updated from 0.5 -> 0.6

--- irkalla/site-lisp/base/init-elpaca.el
+++ irkalla/site-lisp/base/init-elpaca.el
@@ -12,13 +12,13 @@
 ;;; Code:
 
 ;; Bootstrapping Elpaca
-(defvar elpaca-installer-version 0.5)
+(defvar elpaca-installer-version 0.6)
 (defvar elpaca-directory (expand-file-name "var/elpaca/" user-emacs-directory))
 (defvar elpaca-builds-directory (expand-file-name "builds/" elpaca-directory))
 (defvar elpaca-repos-directory (expand-file-name "repos/" elpaca-directory))
 (defvar elpaca-order '(elpaca :repo "https://github.com/progfolio/elpaca.git"
                               :ref nil
-                              :files (:defaults (:exclude "extensions"))
+                              :files (:defaults "elpaca-test.el" (:exclude "extensions"))
                               :build (:not elpaca--activate-package)))
 (let* ((repo  (expand-file-name "elpaca/" elpaca-repos-directory))
        (build (expand-file-name "elpaca/" elpaca-builds-directory))
