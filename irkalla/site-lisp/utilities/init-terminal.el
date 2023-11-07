edc98b599831d78d4307253dd542d6ccef4983d8
Icy-Thought <icy-thought@pm.me>
2023-11-07 07:53:35 +0100

    [𝚫] (irkalla): multi-vterm + changing bindings

--- irkalla/site-lisp/utilities/init-terminal.el
+++ irkalla/site-lisp/utilities/init-terminal.el
@@ -20,8 +20,8 @@
            "<S-next>"  #'scroll-up-command)
 
   (irkalla/comma-lead-keydef
-    "t"   '(:ignore t       :which-key "Terminal")
-    "t c" '(vterm-copy-mode :which-key "Terminal -> read-only"))
+    "t"   '(:ignore t              :which-key "Terminal")
+    "t y" '(vterm-copy-mode        :which-key "Copy contents from VTerm"))
   :custom
   (vterm-timer-delay 0.01)
   (vterm-max-scrollback 10000)
@@ -32,10 +32,23 @@
   :commands (vterm-toggle vterm-toggle-cd)
   :general
   (irkalla/space-lead-keydef
-    "t"   '(:ignore t       :which-key "Terminal")
-    "t t" '(vterm-toggle    :which-key "Open VTerm")
-    "t c" '(vterm-toggle-cd :which-key "Open VTerm"))
+    "t"   '(:ignore t              :which-key "Terminal")
+    "t t" '(vterm-toggle           :which-key "Toggle Terminal"))
+  (irkalla/comma-lead-keydef
+    "t c" '(vterm-toggle-insert-cd :which-key "cd project-dir"))
   :custom (vterm-toggle-fullscreen-p t))
+
+(use-package multi-vterm
+  :requires (vterm)
+  :commands (multi-vterm)
+  :general
+  (irkalla/space-lead-keydef
+    "t n" '(multi-vterm                  :which-key "New Terminal")
+    "t j" '(multi-vterm-next             :which-key "Next")
+    "t k" '(multi-vterm-prev             :which-key "Previous")
+    "t d" '(multi-vterm-dedicated-toggle :which-key "Dedicated Terminal")
+    "t r" '(multi-vterm-projectile       :which-key "Projectile Terminal"))
+  :custom (multi-vterm-buffer-name "VTerm"))
 
 (provide 'init-terminal)
 ;;; init-terminal.el ends here
