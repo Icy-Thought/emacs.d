a6c1737e95e100fe50de0659fd6513a416e1e0e5
Icy-Thought <icy-thought@pm.me>
2023-12-14 01:57:14 +0100

    [𝚫] (irkalla): hooking up `typst-lsp` to eglot

--- irkalla/site-lisp/coding/langspec/init-typst.el
+++ irkalla/site-lisp/coding/langspec/init-typst.el
@@ -14,8 +14,13 @@
 (use-package typst-mode
   :elpaca (:host sourcehut :repo "meow_king/typst-ts-mode")
   :mode ("\\.typ\\'" . typst-ts-mode)
+  :hook (tyspt-ts-mode . eglot-ensure)
   :custom (typst-ts-mode-watch-options "--open")
   :config
+  (when (executable-find "typst-lsp")
+    (with-eval-after-load 'eglot
+      (add-to-list 'eglot-server-programs '(typst-ts-mode . ("typst-lsp")))))
+
   (with-eval-after-load 'consult
     (setopt consult-imenu-config
             (append consult-imenu-config '((typst-ts-mode
