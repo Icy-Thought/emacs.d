9db774c4f063ef435f1434daceedca8d26816ecf
Icy-Thought <icy-thought@pm.me>
2024-02-15 23:04:39 +0100

    [𝚫] (irkalla): minor rearrangement + defun -> direct

--- irkalla/early-init.el
+++ irkalla/early-init.el
@@ -17,22 +17,20 @@
   :group 'emacs)
 
 ;; :NOTE| appending UI changes early to Emacs
-(setq-default default-frame-alist
-              (append (list
-                       '(alpha-background     . 85)
-                       '(fullscreen           . nil)
-                       '(menu-bar-lines       . 0)
-                       '(tool-bar-lines       . 0)
-                       '(vertical-scroll-bars . nil)))
-              initial-frame-alist (copy-alist default-frame-alist))
+(setq-default default-frame-alist (append (list
+                                           '(alpha-background     . 85)
+                                           '(fullscreen           . nil)
+                                           '(menu-bar-lines       . 0)
+                                           '(tool-bar-lines       . 0)
+                                           '(vertical-scroll-bars . nil)))
+              initial-frame-alist (copy-alist default-frame-alist)
+              user-emacs-directory "~/.config/emacs")
 
 ;; :NOTE| Providing quicker access to Irkalla's directories 
 (defcustom irkalla/main-residence (file-truename "~/Workspace/public/emacs.d/irkalla")
   "The directory where Irkalla resides in."
   :type 'string
   :group 'irkalla)
-
-(setq-default user-emacs-directory "~/.config/emacs")
 
 (defvar irkalla/completion-dir  (concat irkalla/main-residence "/site-lisp/completion"))
 (defvar irkalla/decorations-dir (concat irkalla/main-residence "/site-lisp/decorations"))
@@ -41,18 +39,12 @@
 (defvar irkalla/utilities-dir   (concat irkalla/main-residence "/site-lisp/utilities"))
 
 ;; :NOTE| adding our directories to the Emacs load-path
-(defun add-subdirs-to-load-path (dir)
-  "Recursive add directories to our Emacs `load-path'."
-  (let ((default-directory (file-name-as-directory dir)))
-    (add-to-list 'load-path dir)
+(let ((default-directory irkalla/main-residence))
+  (when (file-exists-p default-directory)
+    (normal-top-level-add-to-load-path '("."))
     (normal-top-level-add-subdirs-to-load-path)))
-(add-subdirs-to-load-path irkalla/main-residence)
 
 (add-to-list 'custom-theme-load-path (concat irkalla/decorations-dir "/themes"))
-
-;; Early package modifications
-(require 'init-performance)
-(require 'init-options)
 
 ;; :NOTE| Changing the behaviour of custom.el
 (setq-default custom-file (expand-file-name "etc/custom.el" user-emacs-directory))
@@ -64,5 +56,9 @@
 ;; Prevent certain buffers from being killed
 (with-current-buffer "*scratch*"  (emacs-lock-mode 'kill))
 (with-current-buffer "*Messages*" (emacs-lock-mode 'kill))
+
+;; Early package modifications
+(require 'init-performance)
+(require 'init-options)
 
 ;;; early-init.el ends here
