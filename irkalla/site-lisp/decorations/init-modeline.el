7333d569f5749872506ca5d43c97b4aae4947fc8
Icy-Thought <icy-thought@pm.me>
2024-03-07 19:11:48 +0100

    Irkalla: +mini-echo (enable <- icon support)

--- irkalla/site-lisp/decorations/init-modeline.el
+++ irkalla/site-lisp/decorations/init-modeline.el
@@ -23,6 +23,22 @@
   (doom-modeline-height 35)
   (when (display-graphic-p) (doom-modeline-hud t)))
 
+(use-package mini-echo
+  :disabled t
+  :after (nerd-icons)
+  :hook (elpaca-after-init . mini-echo-mode)
+  :custom
+  (mini-echo-default-segments
+   '( :long ("evil" "major-mode" "vcs" "buffer-position" "envrc"
+             "buffer-size" "flymake" "process" "selection-info"
+             "narrow" "macro" "profiler" "repeat")
+      :short ("evil" "major-mode" "buffer-position"
+              "flymake" "process" "selection-info" "narrow"
+              "macro" "profiler" "repeat")))
+  (mini-echo-separator " ")
+  (mini-echo-update-interval 0.2)
+  (mini-echo-right-padding 1))
+
 (use-package telephone-line
   :disabled t
   :hook ((prog-mode text-mode) . telephone-line-mode)
